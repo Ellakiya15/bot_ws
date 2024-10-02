84e65d90d21b96313867ff58799f59e931afaca3
Ellakiya15 <ellakiyaarun@gmail.com>
2024-10-02 16:53:10 +0530

    Added Bookstore world

--- src/bot/launch/world.launch.py
+++ src/bot/launch/world.launch.py
@@ -1,20 +1,35 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    pkg_gazebo_ros = os.path.join(os.environ['AMENT_PREFIX_PATH'].split(':')[0], 'share', 'gazebo_ros')
-    world_file_path = '/home/ellakiya/ros2_ws/src/Dataset-of-Gazebo-Worlds-Models-and-Maps/worlds/office.world'
+    # Get the path to the world file
+    world_file = os.path.join(
+        get_package_share_directory('bot'),
+        'world',
+        'bookstore.world'
+    )
+    
+    # Get the path to the models directory
+    models_dir = os.path.join(
+        get_package_share_directory('bot'),
+        'models'
+    )
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
-        ),
-        launch_arguments={'world': world_file_path}.items(),
+    # Set the GAZEBO_MODEL_PATH to include the models directory
+    gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=models_dir
+    )
+
+    # Launch Gazebo with the world file
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', '--verbose', world_file],
+        output='screen'
     )
 
     return LaunchDescription([
-        gazebo
+        gazebo_model_path,  # Set the models path
+        gazebo,             # Launch Gazebo
     ])
