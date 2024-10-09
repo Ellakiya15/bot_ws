6b01957fdbd5a475a93c176099023c411e9be345
Ellakiya15 <ellakiyaarun@gmail.com>
2024-10-09 18:00:09 +0530

    worked in world file

---
+++ src/bot/launch/no_roof_warehouse_world.launch.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot = get_package_share_directory('bot')
+    install_dir = get_package_prefix('bot')
+    gazebo_models_path = os.path.join(pkg_box_bot, 'models')
+    world_directory = os.path.join(pkg_box_bot,'world','no_roof_small_warehouse.world')
+    urdf_file = os.path.join(pkg_box_bot, 'urdf', 'trial.urdf')
+    
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share'
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =install_dir + "/share" + ':' + gazebo_models_path
+    
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+    
+    
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+    
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value=[os.path.join(pkg_box_bot, 'world', 'no_roof_small_warehouse.world')],
+            description='SDF world file'),
+        
+        ExecuteProcess(
+            cmd=['gazebo', [world_directory], '-s', 'libgazebo_ros_factory.so', '-s', 'libgazebo_ros_init.so'],
+            output='screen'
+        ),
+    ])
--- src/bot/launch/world.launch.py
+++ src/bot/launch/world.launch.py
@@ -1,35 +1,50 @@
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable
-from ament_index_python.packages import get_package_share_directory
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    # Get the path to the world file
-    world_file = os.path.join(
-        get_package_share_directory('bot'),
-        'world',
-        'bookstore.world'
-    )
-    
-    # Get the path to the models directory
-    models_dir = os.path.join(
-        get_package_share_directory('bot'),
-        'models'
-    )
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_bot = get_package_share_directory('bot')
+    install_dir = get_package_prefix('bot')
+    gazebo_models_path = os.path.join(pkg_bot, 'models')
+    world_directory = os.path.join(pkg_bot,'world','no_roof_small_warehouse.world')
+    urdf_file = os.path.join(pkg_bot, 'urdf', 'trial.urdf')
 
-    # Set the GAZEBO_MODEL_PATH to include the models directory
-    gazebo_model_path = SetEnvironmentVariable(
-        name='GAZEBO_MODEL_PATH',
-        value=models_dir
-    )
+    # Set GAZEBO_MODEL_PATH and GAZEBO_PLUGIN_PATH
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share'
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = install_dir + "/share" + ':' + gazebo_models_path
 
-    # Launch Gazebo with the world file
-    gazebo = ExecuteProcess(
-        cmd=['gazebo', '--verbose', world_file],
-        output='screen'
-    )
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
 
     return LaunchDescription([
-        gazebo_model_path,  # Set the models path
-        gazebo,             # Launch Gazebo
+        # Launch the Gazebo simulator with the specified world
+        DeclareLaunchArgument(
+            'world',
+            default_value=[os.path.join(pkg_bot, 'world', 'no_roof_small_warehouse.world')],
+            description='SDF world file'),
+
+        ExecuteProcess(
+            cmd=['gazebo', world_directory, '-s', 'libgazebo_ros_factory.so', '-s', 'libgazebo_ros_init.so'],
+            output='screen'
+        ),
+
+        # Spawn the robot in Gazebo
+        Node(
+            package='gazebo_ros', executable='spawn_entity.py',
+            arguments=['-file', urdf_file, '-entity', 'your_robot_name'],
+            output='screen'
+        )
     ])
